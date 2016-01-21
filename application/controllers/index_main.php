0ffe009f43cf3152b7d34254395372efad1a7e56
MR.M <472302030@qq.com>
2016-01-21 14:25:53 +0800

    1.21科室开发

--- application/controllers/index_main.php
+++ application/controllers/index_main.php
@@ -5,11 +5,17 @@ class Index_main extends CI_Controller //前台主界面控制器，控制所有
 	public function __construct()      //构造函数，用以自加载方法
 	{
 		parent :: __construct();
+		$this->load->model('main_model','main');   //默认载入数据库模型
 	}
 	
 	public function index()
 	{
 		$this->load->view('index/index.html');
+	}
+	
+	public function main()            //加载主界面视图
+	{
+		$this->load->view('index/main.html');
 	}
 	
 	
